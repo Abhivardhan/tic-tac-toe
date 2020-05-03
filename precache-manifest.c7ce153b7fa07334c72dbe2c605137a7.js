9e35b59351f08a43b9f5b900ae2ad984637ff442
Abhivardhan <saddikuti.abhivardhan@gmail.com>
2020-05-03 13:43:13 +0530

    Updates

---
+++ precache-manifest.c7ce153b7fa07334c72dbe2c605137a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6240bd4b30f5fa336d891718a6263435",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "0701056875a5796eb1dd",
+    "url": "/tic-tac-toe/static/css/main.9a0e43c0.chunk.css"
+  },
+  {
+    "revision": "659c0a7a2e8c64e1027d",
+    "url": "/tic-tac-toe/static/js/2.0c1511cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.0c1511cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0701056875a5796eb1dd",
+    "url": "/tic-tac-toe/static/js/main.0f3a1b8a.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
